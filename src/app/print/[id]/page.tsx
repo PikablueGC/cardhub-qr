9e991efe823589e0c3b9944b42ea3d92739c6bef
CheCortez <pikabluegc@gmail.com>
2025-04-18 03:08:43 +1000

    Update page.tsx

--- src/app/print/[id]/page.tsx
+++ src/app/print/[id]/page.tsx
@@ -3,7 +3,6 @@
 
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
-import Image from 'next/image';
 
 // Simple data type for print jobs
 type PrintData = {
